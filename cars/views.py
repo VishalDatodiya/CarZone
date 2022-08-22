69186444ee246530079ebd0e0ec1099076b8e29a
vishal datodiya <vish@gmail.com>
2022-08-22 23:01:48 +0530

    completed search functionality and admin customization

--- cars/views.py
+++ cars/views.py
@@ -9,11 +9,22 @@ def cars(request):
     paginator = Paginator(cars,4)
     page = request.GET.get('page')
     paged_car = paginator.get_page(page)
+
+    # cars small search pages
+    model_search = Car.objects.values_list('model',flat=True).distinct()
+    year_search = Car.objects.values_list('year',flat=True).distinct()
+    city_search = Car.objects.values_list('city',flat=True).distinct()
+    body_style_search = Car.objects.values_list('body_style',flat=True).distinct()
+
     data = {
         # we use this below (commenetd) line when we are feching the data for cars but when we fetching the pagination
         # so use override the cars to paged_car in context
         # 'cars' : paged_car,
         'cars' : paged_car,
+        'model_search': model_search,
+        'year_search':year_search,
+        'city_search':city_search,
+        'body_style_search':body_style_search,
     }
 
     return render(request,'cars/cars.html',data)
@@ -25,3 +36,91 @@ def car_details(request,id):
     }
 
     return render(request, 'cars/car_details.html',data)
+
+
+def search(request):
+    cars = Car.objects.order_by('-created_date')
+
+    model_search = Car.objects.values_list('model',flat=True).distinct()
+    year_search = Car.objects.values_list('year',flat=True).distinct()
+    city_search = Car.objects.values_list('city',flat=True).distinct()
+    body_style_search = Car.objects.values_list('body_style',flat=True).distinct()
+    transmission_search = Car.objects.values_list('transmission',flat=True).distinct()
+
+
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            cars = cars.filter(description__icontains=keyword)
+    # data = {
+    #     'cars': cars,
+    # }
+
+    # here we are checking the model which is passes from the URL (request)
+
+    # Note : here this model, city .... this name is come from model.py name.
+    # in min_price you have to use price bcz you used price as a variable name in models.py
+
+    if 'model' in request.GET:
+        model = request.GET['model']
+        if model:
+            cars = cars.filter(model__iexact=model)
+    # data = {
+    #     'cars': cars,
+    # }
+
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            cars = cars.filter(city__iexact=city)
+    # data = {
+    #     'cars': cars,
+    # }
+
+    if 'year' in request.GET:
+        year = request.GET['year']
+        if year:
+            cars = cars.filter(year__iexact=year)
+    # data = {
+    #     'cars': cars,
+    # }
+
+    if 'body_style' in request.GET:
+        body_style = request.GET['body_style']
+        if body_style:
+            cars = cars.filter(body_style__iexact=body_style)
+    # data = {
+    #     'cars': cars,
+    # }
+
+    if 'transmission' in request.GET:
+        transmission = request.GET['transmission']
+        if transmission:
+            cars = cars.filter(transmission__iexact=transmission)
+
+    # data = {
+    #     'cars': cars,
+    # }
+
+    
+    # min and max price
+
+
+    if 'min_price' in request.GET:
+        min_price = request.GET['min_price']
+        max_price = request.GET['max_price']
+        if max_price:
+            cars = cars.filter(price__gte=min_price, price__lte=max_price)
+
+
+
+    data = {
+        'cars': cars,
+        'model_search': model_search,
+        'year_search':year_search,
+        'city_search':city_search,
+        'body_style_search':body_style_search,
+        'transmission_search':transmission_search,
+    }
+
+    return render(request,'cars/search.html',data)
--- pages/views.py
+++ pages/views.py
@@ -8,10 +8,23 @@ def home(request):
     teams = Team.objects.all()
     is_featured = Car.objects.order_by('-created_date').filter(is_featured=True)
     all_cars = Car.objects.order_by('-created_date')
+
+    # search form dropdown values./option.
+    # search_fields = Car.objects.values('model','city','year','body_style')   # this is not giving unique values.
+    model_search = Car.objects.values_list('model',flat=True).distinct()
+    city_search = Car.objects.values_list('city',flat=True).distinct()
+    year_search = Car.objects.values_list('year',flat=True).distinct()
+    body_style_search = Car.objects.values_list('body_style',flat=True).distinct()
+
     data = {
         'teams': teams,
         'is_featured': is_featured,
         'all_cars': all_cars,
+        # 'search_fields': search_fields,
+        'model_search':model_search,
+        'city_search':city_search,
+        'year_search':year_search,
+        'body_style_search':body_style_search,
     }
     return render(request,'pages/home.html',data)
 
